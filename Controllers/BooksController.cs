7d48fc8d8a140f540f2d2daa2ed351cd79cf8998
Danielle DuLong <danielledulong@gmail.com>
2021-12-09 13:29:07 -0500

    Add Status and Due Date to Book Model and Remove NumberInStock

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -57,7 +57,7 @@ namespace FinalProject.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Author,NumberInStock,GenreId")] Book book)
+        public async Task<IActionResult> Create([Bind("Id,Title,Author,Status,GenreId")] Book book)
         {
             if (ModelState.IsValid)
             {
@@ -91,7 +91,7 @@ namespace FinalProject.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Author,NumberInStock,GenreId")] Book book)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Author,Status,GenreId")] Book book)
         {
             if (id != book.Id)
             {
