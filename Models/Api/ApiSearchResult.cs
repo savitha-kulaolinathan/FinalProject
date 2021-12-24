46660fcc6bbc4798b435dca13d39e20f8f7edb42
Danielle DuLong <danielledulong@gmail.com>
2021-12-23 21:33:10 -0500

    Add APISearchResultViewModel

--- Models/Api/ApiSearchResult.cs
+++ Models/Api/ApiSearchResult.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalProject.Models.ViewModels;
 
 namespace FinalProject.Models.Api
 {
     public class ApiSearchResult
     {
         public string keyword { get; set; }
+        public string error { get; set; }
+        public string total { get; set; }
+        public string page { get; set; }
+
+        public IEnumerable<ApiSearchResultViewModel> books { get; set; }
     }
 }
---
+++ Models/ViewModels/ApiSearchResultViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models.ViewModels
+{
+    public class ApiSearchResultViewModel
+    {
+        public string title { get; set; }
+        public string subtitle { get; set; }
+        public string image { get; set; }
+        public string Description { get; set; }
+        public string isbn13 { get; set; }
+        public string url { get; set; }
+    }
+}
