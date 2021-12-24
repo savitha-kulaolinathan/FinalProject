f6dd15053081656311e58068f5fe1be7bfb82107
Danielle DuLong <danielledulong@gmail.com>
2021-12-23 21:28:38 -0500

    Create ApiSearchResult & View

--- Controllers/Api/ApiController.cs
+++ Controllers/Api/ApiController.cs
@@ -2,15 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using FinalProject.Models.Api;
 
 namespace FinalProject.Controllers.Api
 {
     public class ApiController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _client; 
+
+        public ApiController(HttpClient client)
         {
-            return View();
+            _client = client;
+        }
+
+        public async Task<IActionResult> SearchAPI(string keyword)
+        {
+
+            var streamTask = _client.GetStreamAsync("https://api.itbook.store/1.0/search/" + keyword);
+            var result = await JsonSerializer.DeserializeAsync<ApiSearchResult>(await streamTask);
+            result.keyword = keyword;
+
+            return View("ApiSearchResult", result);
+
         }
     }
 }
---
+++ Models/Api/ApiSearchResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models.Api
+{
+    public class ApiSearchResult
+    {
+        public string keyword { get; set; }
+    }
+}
